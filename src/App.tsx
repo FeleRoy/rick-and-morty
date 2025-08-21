a710b1883f5571bbb4267c1cd0fe61e6ca22549d
Baratov Viktor <vitya.baratov@gmail.com>
2025-08-21 18:14:02 +0300

    feat: собрал форму

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,11 @@
-import DropDown from "./shared/ui/DropDown/DropDown"
-import Input from "./shared/ui/Input/Input"
+import CharacterSearchForm from "./shared/CharacterSearchForm/CharacterSearchForm"
 
 function App() {
   return (
     <>
     <div className="bg-black font-comic text-white h-screen">
       <h1>Hello world!</h1>
-      <Input
-        label="Имя персонажа"
-        onChange={() => {}}
-        inputId="name"
-        placeholder="Введите имя персонажа"
-      />
-      <DropDown title={"Жив?"} options={["Жив", "Мёртв"]} onSelect={()=>{}}></DropDown>
+      <CharacterSearchForm/>
     </div>
     </>
   )
--- src/shared/CharacterSearchForm/CharacterSearchForm.tsx
+++ src/shared/CharacterSearchForm/CharacterSearchForm.tsx
@@ -1,19 +1,34 @@
-import React from 'react';
+import React from "react";
+import Input from "../ui/Input/Input";
+import DropDown from "../ui/DropDown/DropDown";
 
-interface CharacterSearchFormProps {
-
-}
+interface CharacterSearchFormProps {}
 
 const CharacterSearchForm: React.FC<CharacterSearchFormProps> = () => {
+  const handleSubmit = (e: React.FormEvent) => {
+    e.preventDefault();
+  };
 
-    const handleSubmit = (e: React.FormEvent) => {
-        e.preventDefault();
-    };
-
-    return (
-        <form onSubmit={handleSubmit}>
-        </form>
-    );
+  return (
+    <form onSubmit={handleSubmit}>
+      <Input
+        label="Имя персонажа"
+        onChange={() => {}}
+        inputId="name"
+        placeholder="Введите имя персонажа"
+      />
+      <fieldset className="flex justify-between">
+        <DropDown title={"Жив?"} options={["Жив", "Мёртв", "Неизвестно"]} onSelect={()=>{}}></DropDown>
+        <DropDown title={"Раса?"} options={["ЗАПОЛНИТЬ", "РАСЫ"]} onSelect={()=>{}}></DropDown>
+      </fieldset>
+      <Input
+        label="Эпизод"
+        onChange={() => {}}
+        inputId="episode"
+        placeholder="Введите название эпизода"
+      />
+    </form>
+  );
 };
 
 export default CharacterSearchForm;
