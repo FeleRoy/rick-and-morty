f6664d1372013b761b68b99b1d3cdbb94e0ddcf4
Baratov Viktor <vitya.baratov@gmail.com>
2025-08-22 23:00:30 +0300

    feat: начал настраивать стрелки поиска

--- src/pages/HomePage/ui/CharacterSearchSection/CharacterSearchSection.tsx
+++ src/pages/HomePage/ui/CharacterSearchSection/CharacterSearchSection.tsx
@@ -1,35 +1,38 @@
-import React from "react";
+import React, { useState } from "react";
 import CharacterSearchForm from "../CharacterSearchForm/CharacterSearchForm";
 import type { Character, CharacterFilter } from "../../../../utils/types";
 import { getCharacters } from "../../../../utils/Api";
-import SearchResult from "../SearchResult/SearchResult";
-
+import SearchResult, { type pagesInfoType } from "../SearchResult/SearchResult";
 
 interface CharacterSearchSectionProps {}
 
-
-const CharacterSearchSection: React.FC<CharacterSearchSectionProps> = ({}) =>{
-      const [characters, setCharacters] = React.useState<Character[]>([])
-
-      const handleFormChange = (data: CharacterFilter)=>{
-        // TODO: реализовать подгрузку элементов с других страниц
-        getCharacters(data)
-          .then(response => {
-            setCharacters(response.results);
-          })
-          .catch(error => {
-            setCharacters([]);
-            console.error('Ошибка запроса персонажей:', error);
-          });
-      }
-    return(
-        <div className="border border-solid max-w-2xl mx-auto p-3 rounded-3xl flex flex-col mb-5">
-            <h1 className="text-3xl mb-4">Вселенная Рик и Морти</h1>
-            <CharacterSearchForm onChange={handleFormChange}/>
-            <h2 className="text-2xl my-4">Найдено</h2>
-            <SearchResult characters={characters}/>
-        </div>
-    );
-}
+const CharacterSearchSection: React.FC<CharacterSearchSectionProps> = ({}) => {
+  const [characters, setCharacters] = useState<Character[]>([]);
+  const [pageInfo, setPageInfo] = useState<pagesInfoType>({
+    prevPage: null,
+    currentPage: "1",
+    nextPage: null,
+  });
+  const handleFormChange = (data: CharacterFilter) => {
+    // TODO: реализовать подгрузку элементов с других страниц
+    getCharacters(data)
+      .then((response) => {
+        setPageInfo({...pageInfo, prevPage: response.info.prev, nextPage: response.info.next})
+        setCharacters(response.results);
+      })
+      .catch((error) => {
+        setCharacters([]);
+        console.error("Ошибка запроса персонажей:", error);
+      });
+  };
+  return (
+    <div className="border border-solid max-w-2xl mx-auto p-3 rounded-3xl flex flex-col mb-5">
+      <h1 className="text-3xl mb-4">Вселенная Рик и Морти</h1>
+      <CharacterSearchForm onChange={handleFormChange} />
+      <h2 className="text-2xl my-4">Найдено</h2>
+      <SearchResult pageInfo={pageInfo} characters={characters} />
+    </div>
+  );
+};
 
 export default CharacterSearchSection;
